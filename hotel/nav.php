57a412454651ff7c8d6dfabe53e420d8043a11a0
Rashik I Nahian <rashik@nahian.me>
2018-08-10 22:33:28 +0600

    hotel login  added

---
+++ hotel/nav.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="">Hotel Booking</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+           
+            <li><a href="">Home</a></li>
+       
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span>&nbsp; <?php echo $userRow['email']; ?></a></li>
+            <li><a href="logout.php?logout"><span class="glyphicon glyphicon-log-out"></span>&nbsp; Logout</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
---
+++ hotel/register.php
@@ -0,0 +1,98 @@
+<?php
+session_start();
+if (isset($_SESSION['userSession'])!="") {
+	header("Location: home.php");
+}
+require_once '../dbconnect.php';
+
+if(isset($_POST['btn-signup'])) {
+	
+	
+	$email = strip_tags($_POST['email']);
+	$upass = strip_tags($_POST['password']);
+	
+	
+	$email = $DBcon->real_escape_string($email);
+	$upass = $DBcon->real_escape_string($upass);
+	
+	$hashed_password = password_hash($upass, PASSWORD_DEFAULT); // this function works only in PHP 5.5 or latest version
+	
+	$check_email = $DBcon->query("SELECT email FROM hotels WHERE email='$email'");
+	$count=$check_email->num_rows;
+	
+	if ($count==0) {
+		
+		$query = "INSERT INTO hotels(email,password) VALUES('$email','$hashed_password')";
+
+		if ($DBcon->query($query)) {
+			$msg = "<div class='alert alert-success'>
+						<span class='glyphicon glyphicon-info-sign'></span> &nbsp; successfully registered !
+					</div>";
+		}else {
+			$msg = "<div class='alert alert-danger'>
+						<span class='glyphicon glyphicon-info-sign'></span> &nbsp; error while registering !
+					</div>";
+		}
+		
+	} else {
+		
+		
+		$msg = "<div class='alert alert-danger'>
+					<span class='glyphicon glyphicon-info-sign'></span> &nbsp; sorry email already taken !
+				</div>";
+			
+	}
+	
+	$DBcon->close();
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <?php include("head.php");?>
+</head>
+<body>
+
+<div class="signin-form">
+
+	<div class="container">
+     
+        
+       <form class="form-signin" method="post" id="register-form">
+      
+        <h2 class="form-signin-heading">Sign Up</h2><hr />
+        
+        <?php
+		if (isset($msg)) {
+			echo $msg;
+		}
+		?>
+          
+      
+        
+        <div class="form-group">
+        <input type="email" class="form-control" placeholder="Email address" name="email" required  />
+        <span id="check-e"></span>
+        </div>
+        
+        <div class="form-group">
+        <input type="password" class="form-control" placeholder="Password" name="password" required  />
+        </div>
+        
+     	<hr />
+        
+        <div class="form-group">
+            <button type="submit" class="btn btn-default" name="btn-signup">
+    		<span class="glyphicon glyphicon-log-in"></span> &nbsp; Create Account
+			</button> 
+            <a href="index.php" class="btn btn-default" style="float:right;">Log In Here</a>
+        </div> 
+      
+      </form>
+
+    </div>
+    
+</div>
+
+</body>
+</html>
